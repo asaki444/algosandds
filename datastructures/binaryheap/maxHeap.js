035be6ce036302f95156a89bf0b967e7789ccda5
Aditi <asaki444@gmail.com>
2020-06-16 02:22:11 -0400

    binary heaps lesson

---
+++ datastructures/binaryheap/maxHeap.js
@@ -0,0 +1,70 @@
+class MaxBinaryHeap {
+    constructor(){
+        this.values  = [41,39,33,18,27,12,55]
+    }
+
+    insert(element){
+        this.values.push(element)
+        this.bubbleUp()
+    }
+
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+                this.values[parentIdx] = element;
+                this.values[idx] = parent;
+                idx = parentIdx;
+        }
+
+    }
+
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop();
+
+        this.sinkDown();
+
+        return max;
+    }
+
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let element = this.values[0];
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx]
+                if(leftChild > element){
+                    swap = leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx < length){
+                rightChild = this.values[leftChildIdx];
+                if(rightChild > element){
+                    swap = rightChildIdx
+                }
+            }
+            
+            if(swap === null) break;
+           }
+    }
+}
+
+let heap = new MaxBinaryHeap();
+
+heap.insert(55);
+heap.insert(199);
+
+console.log(heap)
